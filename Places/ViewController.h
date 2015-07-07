81f2e7eb3a80457a4bf30dfc86c539ed695b9b99
iOS1 <ios1@serbans-mbp-2.ad.qualitance.com>
2015-07-07 11:46:49 +0300

    Initial Commit

---
+++ Places/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Places
+//
+//  Created by iOS1 on 07/07/15.
+//  Copyright (c) 2015 Brindusa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
